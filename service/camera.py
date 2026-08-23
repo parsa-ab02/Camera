cf430775f69f2271e325debfa91efe81b37a2453
parsa-ab02 <parsaabedi02@gmail.com>
2026-08-23 05:12:19 +0330

    use openCV instead of v4l2-ctl and ffmpeg

--- service/camera.py
+++ service/camera.py
@@ -1,30 +1,67 @@
-import subprocess
-from pathlib import Path
+import cv2
+import threading
+import time
 
 class Camera:
-    video_device: Path
+    _camera: cv2.VideoCapture
+    _is_recording: bool
+    _is_streaming: bool
 
-    def __init__(self,video_device: Path):
-        self.video_device = video_device
+    _video: list
+    _history: list
 
-    def stream(self):
-        ...
+    def __init__(self, camera_id):
+        self._camera = cv2.VideoCapture(camera_id)
+        self._is_recording = False
+        self._is_streaming = False
 
-    def capture(self, width, height):
-        v4l2_cmd = [
-            "v4l2-ctl",
-            f"--device={self.video_device}",
-            f"--set-fmt-video=width={width},height={height},pixelformat=YUYV",
-            "--stream-mmap",
-            "--stream-count=1",
-            "--stream-to=-",
-        ]
-        capture_image = subprocess.run(v4l2_cmd,stdout=subprocess.PIPE,check=True,)
+        self._video = []
+        self._history = []
 
-        return capture_image.stdout
+    def run(self, width, height, fps):
+        self.streamThread = threading.Thread(target=self._stream, args=(width, height, fps))
 
-    def start_record(self):
-        ...
+        self.streamThread.start()
 
-    def stop_record(self):
-        ...
+    def _stream(self, width, height, fps):
+        self._is_streaming = True
+
+        frame_time = 1 / fps
+
+        while self._is_streaming:
+            start = time.perf_counter()
+
+            ret, frame = self._camera.read()
+            if not ret:
+                raise Exception
+
+            if self._is_recording:
+                self._video.append((ret, frame))
+
+            cv2.imshow("camera", frame)
+
+            delay = frame_time - (time.perf_counter()-start)
+
+            if  delay > 0:
+                time.sleep(delay)
+
+        return
+
+    def quit(self):
+        self._is_streaming = False
+
+    def capture(self):
+        ret, image = self._camera.read()
+
+        if not ret:
+            raise Exception
+
+        self._history.append(image)
+
+    def record(self):
+        self._is_recording = True
+
+    def end_record(self):
+        self._is_recording = False
+        self._history.append(self._video)
+        self._video = []
