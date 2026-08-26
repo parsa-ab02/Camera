e65c34631ea42809dbdb0df9277fcc0b6d706fde
parsa-ab02 <parsaabedi02@gmail.com>
2026-08-26 14:53:21 +0330

    add proper getters

--- service/cameraControl.py
+++ service/cameraControl.py
@@ -2,8 +2,10 @@ import cv2
 import Camera
 
 class CameraControl:
-    def __init__(self, camera:Camera):
+    def __init__(self, camera: Camera):
         self._camera = camera
+
+    # BRIGHTNESS --- --- ---
 
     def set_brightness(self, value: int): # range 0 - 255
         if not 0 <= value <= 255:
@@ -15,6 +17,13 @@ class CameraControl:
         )
 
         return
+
+    def get_brightness(self)->int:
+        return self._camera.get_property(
+            cv2.CAP_PROP_BRIGHTNESS
+        )
+
+    # CONTRAST --- --- ---
     
     def set_contrast(self, value: int): # range 0 - 255
         if not 0 <= value <= 255:
@@ -25,6 +34,15 @@ class CameraControl:
             value
         )
 
+        return
+
+    def get_contrast(self)->int:
+        return self._camera.get_property(
+            cv2.CAP_PROP_CONTRAST
+        )
+
+    # SATURATION --- --- ---
+
     def set_saturation(self, value: int): # range 0 - 100
         if not 0 <= value <= 100:
             raise Exception("invalid saturation value")
@@ -33,6 +51,13 @@ class CameraControl:
             cv2.CAP_PROP_SATURATION,
             value
         )
+
+    def get_saturation(self)->int:
+        return self._camera.get_property(
+            cv2.CAP_PROP_SATURATION
+        )
+
+    # HUE --- --- ---
 
     def set_hue(self, value: int): # range -180 - 180
         if not -180 <= value <= 180:
@@ -43,29 +68,14 @@ class CameraControl:
             value
         )
 
-    def set_white_balance_automatic(self, value: bool): # True or False
-        self._camera.set_property(
-            cv2.CAP_PROP_AUTO_WB,
-            value
+        return
+
+    def get_hue(self)->int:
+        return self._camera.get_property(
+            cv2.CAP_PROP_HUE
         )
 
-    def set_gamma(self, value: int): # range 90 - 150
-        if not 90 <= value <= 150:
-            raise Exception("invalid gamma value")
-                
-        self._camera.set_property(
-            cv2.CAP_PROP_GAMMA,
-            value
-        )
-
-    def set_white_balance_temperature(self, value: int): # range 2800 – 6500
-        if not 2800 <= value <= 6500:
-            raise Exception("invalid white balance temprature value")
-                
-        self._camera.set_property(
-            cv2.CAP_PROP_WB_TEMPERATURE,
-            value
-        )
+    # SHARPNESS --- --- ---
 
     def set_sharpness(self, value: int): # range 0 - 7
         if not 0 <= value <= 7:
@@ -76,11 +86,71 @@ class CameraControl:
             value
         )
 
-    # def set_power_line_frequency(self, value: int): # range 0 - 2
-        #     if not 0 <= value <= 2:
-        #         raise Exception("invalid power line frequency value")
-                    
-        #     self._camera.set_property(
-        #         cv2.CAP_PROP_,
-        #         value
-        #     )
+        return
+
+    def get_sharpness(self)->int:
+        return self._camera.get_property(
+            cv2.CAP_PROP_SHARPNESS
+        )
+
+    # GAMMA --- --- ---
+
+    def set_gamma(self, value: int): # range 90 - 150
+        if not 90 <= value <= 150:
+            raise Exception("invalid gamma value")
+                
+        self._camera.set_property(
+            cv2.CAP_PROP_GAMMA,
+            value
+        )
+
+        return
+
+    def get_gamma(self)->int:
+        return self._camera.get_property(
+            cv2.CAP_PROP_GAMMA
+        )
+
+    # AUTO WHITE BALANCE --- --- ---
+
+    def set_white_balance_automatic(self, value: bool): # True or False
+        self._camera.set_property(
+            cv2.CAP_PROP_AUTO_WB,
+            value
+        )
+
+        self._wb_automatic = bool
+
+    def is_white_balance_automatic(self)->bool:
+        return self._camera.get_property(
+            cv2.CAP_PROP_AUTO_WB
+        )
+
+    # WHITE BALANCE TEMPERATURE --- --- ---
+
+    def set_white_balance_temperature(self, value: int): # range 2800 – 6500
+        if not self._wb_automatic:
+            raise Exception("white balance automatic is set to auto")
+
+        if not 2800 <= value <= 6500:
+            raise Exception("invalid white balance temprature value")
+                
+        self._camera.set_property(
+            cv2.CAP_PROP_WB_TEMPERATURE,
+            value
+        )
+
+        return
+
+    def get_white_balance_temperature(self)->int:
+        return self._camera.get_property(
+            cv2.CAP_PROP_BRIGHTNESS
+        )
+
+    # POWER LINE FREQUENCY --- --- ---
+
+    def set_power_line_frequency(self, value: int): # range 0 - 2
+        ...
+
+    def get_power_line_frequency(self):
+        ...
